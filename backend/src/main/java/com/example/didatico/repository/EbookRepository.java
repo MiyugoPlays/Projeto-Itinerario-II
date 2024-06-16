4a5d8440a39bf88b672718c5a01a0a246685edec
Terry The Terrible <tiago.rocha03@cs.cruzeirodosul.edu.br>
2024-06-16 17:46:05 -0300

    merda arrumada, utilize maven e spring

--- backend/src/main/java/com/example/didatico/repository/EbookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.didatico.repository;
-
-import com.example.didatico.model.Ebook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EbookRepository extends JpaRepository<Ebook, Long> {
-}
